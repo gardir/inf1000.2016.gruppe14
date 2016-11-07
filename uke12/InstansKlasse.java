985f3ae5293bd10236cc47c8a0f38fc0963a61fe
gardir <gardiz@gmail.com>
2016-11-07 17:08:15 +0100

    Lagt til "InstansKlasse'n"

---
+++ uke12/InstansKlasse.java
@@ -0,0 +1,13 @@
+public class InstansKlasse {
+
+	public void instansPublic() {
+		System.out.println("instansPublic()");
+	}
+
+	public static void instansStaticPublic() {
+		System.out.println("instansStaticPublic()");
+		EksempelKlasse.publicStatic();
+		//EksempelKlasse.privatStatic();
+	}
+	
+}
