9233aa1fc4957d7b114dd4b46fee92b009591e7a
gardir <gardiz@gmail.com>
2016-11-07 15:23:50 +0100

    Oppdatert public/static/private/ikke static eksempelklassene

--- uke12/EksempelKlasse.java
+++ uke12/EksempelKlasse.java
@@ -7,6 +7,30 @@ public class EksempelKlasse {
 
 		System.out.println("Vi har");
 		System.out.println("en linje for alt");
+
+		InstansKlasse ik = new InstansKlasse();
+		EksempelKlasse ek = new EksempelKlasse();
+
+		EksempelKlasse.publicStatic();
+		EksempelKlasse.privatStatic();
+		InstansKlasse.instansStaticPublic();
+		ik.instansPublic();
+	}
+
+	private static void privatStatic() {
+		System.out.println("privatStatic()");
+	}
+
+	private void privat() {
+		System.out.println("privat()");
+	}
+
+	public static void publicStatic() {
+		System.out.println("publicStatic()");
+	}
+
+	public void offentlig() {
+		System.out.println("offentlig()");
 	}
 	
 }
